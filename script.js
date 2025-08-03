e5d4b08d4d0e8c1d8d1141fd515f9af5a9c8a29f
Debisi-o <debisioyewole@gmail.com>
2025-08-03 17:03:31 -0400

    student object

---
+++ script.js
@@ -0,0 +1,12 @@
+// student object
+const student = {
+    name: 'John Doe',
+    age: 22,
+    enrolled: true,
+    courses: ['Math', 'Science', 'History'],
+
+    // Method to display basic info
+    displayInfo() {
+        console.log(`Name: ${this.name}, Age: ${this.age}`);
+    }
+};
